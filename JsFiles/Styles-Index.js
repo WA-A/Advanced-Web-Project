58800172a31dffd72c4b43e2f0b5e9ca4c9ecc66
wasan <wasanawwad2020@gmail.com>
2024-12-03 20:01:00 +0200

    Login Page

---
+++ JsFiles/Styles-Index.js
@@ -0,0 +1,8 @@
+document.querySelector('.signup-button').addEventListener('click', () => {
+    alert('Navigate to the Sign-Up page');
+  });
+  
+  document.querySelector('.login-button').addEventListener('click', () => {
+    window.location.href = 'Login.html';
+  });
+  

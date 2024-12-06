4f3891ca3a3a0bb08c3844da0c6925663e461f31
masaabuaisheh <masaabuaisheh11@gmail.com>
2024-12-06 13:03:10 +0200

    New Updates

--- JsFiles/Styles-Index.js
+++ JsFiles/Styles-Index.js
@@ -1,8 +1,7 @@
-document.querySelector('.signup-button').addEventListener('click', () => {
-    alert('Navigate to the Sign-Up page');
-  });
-  
-  document.querySelector('.login-button').addEventListener('click', () => {
-    window.location.href = 'Login.html';
-  });
-  
+document.querySelector(".signup-button").addEventListener("click", () => {
+  window.location.href = "signUp.html";
+});
+
+document.querySelector(".login-button").addEventListener("click", () => {
+  window.location.href = "Login.html";
+});

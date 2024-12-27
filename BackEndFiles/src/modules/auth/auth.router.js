395063455b739b9552a44f64ad9eb427a6e584b8
wasan <wasanawwad2020@gmail.com>
2024-12-27 22:21:20 +0200

    Sign In

--- BackEndFiles/src/modules/auth/auth.router.js
+++ BackEndFiles/src/modules/auth/auth.router.js
@@ -1,14 +1,11 @@
 import { Router } from "express";
-const router = Router({caseSensitive:true});
 import * as AuthController from './auth.controller.js'
 import { CheckEmail } from "../../MiddleWare/CheckEmail.js";
 import { AsyncHandler } from "../../../utls/CatchError.js";
 import { Validation } from "../../MiddleWare/Validation.js";
 import * as schema from './Auth.Validation.js'
 
-
-
-
+const router = Router({caseSensitive:true});
 
 router.post('/signup',Validation(schema.RegisterSchema),CheckEmail,AsyncHandler(AuthController.SignUp));
 router.post('/signin',Validation(schema.LoginSchema),AuthController.SignIn);

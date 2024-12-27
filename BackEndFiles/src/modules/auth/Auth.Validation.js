1f83abadeaabc7701799334559e6f8aed604107a
wasan <wasanawwad2020@gmail.com>
2024-12-27 22:25:33 +0200

    Validation

--- BackEndFiles/src/modules/auth/Auth.Validation.js
+++ BackEndFiles/src/modules/auth/Auth.Validation.js
@@ -2,13 +2,13 @@ import joi from 'joi';
 
 
 export const RegisterSchema = joi.object({
-    UserName: joi.string().alphanum().min(3).max(30).required(),
+    UserName: joi.string().min(3).max(30).required(),
     Password: joi.string().pattern(/^[A-Z][a-z0-9]{3,20}$/),
     Email: joi.string().email().required(),
 });
 
 
 export const LoginSchema = joi.object({
+    UserName: joi.string().min(3).max(30).required(),
     Password: joi.string().pattern(/^[A-Z][a-z0-9]{3,20}$/),
-    Email: joi.string().email().required(),
 });

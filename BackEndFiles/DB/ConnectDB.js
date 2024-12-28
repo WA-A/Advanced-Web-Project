05435eeb16e32792d7741054daff5107e8332ecf
wasan <wasanawwad2020@gmail.com>
2024-12-28 16:55:09 +0200

    SignIn & SignUp In GraphQL

--- BackEndFiles/DB/ConnectDB.js
+++ BackEndFiles/DB/ConnectDB.js
@@ -1,15 +1,18 @@
-import mongoose from'mongoose';
+// src/utils/connectDB.js
+import mongoose from 'mongoose';
 
-const connectDB = async ()=>{
-
-     mongoose.connect(process.env.DB) 
-     .then( result=>{ 
-console.log(`connected DB`);
-     })
-     .catch(err=>{
-        console.log(`not connected to DB ${err}`);
-     })
-     
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, 
+    });
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err.message);
+    process.exit(1); 
+  }
+};
 
 export default connectDB;

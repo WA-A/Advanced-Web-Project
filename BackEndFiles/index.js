4d9abeed971c6bc21908f7c769868493cd0570f1
wasan <wasanawwad2020@gmail.com>
2024-12-31 01:40:47 +0200

    Add Village

--- BackEndFiles/index.js
+++ BackEndFiles/index.js
@@ -1,22 +1,32 @@
-// index.js
 import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 import dotenv from 'dotenv';
-import connectDB from './DB/ConnectDB.js'; 
-import { typeDefs } from './src/GraphQl/typeDefs.js';
-import { resolvers } from './src/GraphQl/resolvers.js';
+import connectDB from './DB/ConnectDB.js';
+import { typeDefs } from './src/GraphQl/typeDefs.js'; 
+import { resolvers } from './src/GraphQl/resolvers.js'; 
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Connect to database
 connectDB();
 
-const server = new ApolloServer({ typeDefs, resolvers });
+// Create Apollo Server
+const server = new ApolloServer({
+  typeDefs, // مرر التعريفات المدمجة
+  resolvers, // مرر الـ resolvers المدمجة
+  context: ({ req }) => {
+    const token = req.headers.authorization || '';
+    return { token };
+  },
+});
+
 await server.start();
 server.applyMiddleware({ app });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}${server.graphqlPath}`);
 });
---
+++ BackEndFiles/src/GraphQl/VillageResolvers.js
@@ -0,0 +1,36 @@
+import VillageModel from '../Model/Village.Model.js';
+
+export const VillageResolvers = {
+  Mutation: {
+    addVillage: async (_, { Name, Region, LandArea, Latitude, Longitude, ImageUrl, Categories }) => {
+      try {
+        const newVillage = await VillageModel.create({
+          Name,
+          Region,
+          LandArea,
+          Latitude,
+          Longitude,
+          ImageUrl,
+          Categories,
+        });
+        return newVillage;
+      } catch (error) {
+        throw new Error('Error adding village: ' + error.message);
+      }
+    },
+  },
+
+  Query: {
+    getVillages: async () => {
+      return await VillageModel.find();
+    },
+    getVillageById: async (_, { id }) => {
+      return await VillageModel.findById(id);
+    },
+  },
+};
+
+
+
+ 
+  
